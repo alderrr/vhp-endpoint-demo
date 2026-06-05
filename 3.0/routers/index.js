5c58b12cb528fc35584c6063541d762e2ce139a3
alderrr <alifdermayudha@gmail.com>
2026-06-05 13:38:01 +0700

    feat: updated legacy router

--- 3.0/routers/index.js
+++ 3.0/routers/index.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 
 const AuthController = require("../controllers/authController");
 const AmadeusController = require("../controllers/amadeus/amadeusController");
+const LegacyController = require("../controllers/legacy/legacyController");
+
 const authMiddleware = require("../middlewares/authentication");
 
-router.post("/api/v1/security/oauth2/token", AuthController.getToken);
 router.post("/api/v1/admin/client/add", AuthController.createClient);
 router.get("/api/v1/admin/client/all", AuthController.getAllClients);
+router.post("/api/v1/security/oauth2/token", AuthController.getToken);
 
 router.get("/api/v1/events-delivery/health", AmadeusController.healthCheck);
 router.post(
@@ -14,5 +16,8 @@ router.post(
   authMiddleware,
   AmadeusController.handleOtaMessage,
 );
+
+// Legacy
+router.post("/vhpws/htng.xml", LegacyController.handleHtng);
 
 module.exports = router;
